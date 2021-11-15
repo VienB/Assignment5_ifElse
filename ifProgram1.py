75ba77eec132f7001516553433cdae40ca71bfbd
VienB <vienangelobbernales@iskolarngbayan.pup.edu.ph>
2021-11-15 05:47:29 -0800

    attempt

---
+++ ifProgram1.py
@@ -0,0 +1,14 @@
+# Create a program that will ask for grade percentage. Display the equivalent 
+# Grade/Mark and Description
+# Example:
+# Input grade: 87.6
+# Grade/Mark: 1.75
+# Description: Very Good
+
+print("VIEN ANGELO BERNALES|BSCOE 1-1 \n")
+
+
+YourGrade = float(input("Input your grade: "))
+
+if YourGrade > 97 and YourGrade <= 100:
+    print("Grade mark: 1.0", round(YourGrade))
