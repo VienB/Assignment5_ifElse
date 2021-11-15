fc987fd386202f90cbad61be69cd5bb05edc3514
VienB <vienangelobbernales@iskolarngbayan.pup.edu.ph>
2021-11-14 23:02:18 -0800

    I tried to use a function in if else

---
+++ ifProgram1.py
@@ -0,0 +1,26 @@
+# Program 3: Life stages
+# Create a program that ask for an age of a person
+# Display the life stage of the person.
+
+print("VIEN ANGELO BERNALES | BSCOE 1-1 \n")
+
+
+def GetAge():
+    ages = int(input("Please enter your age: "))
+    return ages
+
+if GetAge > -1 and GetAge <= 12:
+    print("KID")
+
+elif GetAge >= 13 and GetAge <= 17:
+    print("TEEN")
+
+elif GetAge == 18:
+    print("DEBUT")
+
+else:
+    print("ADULT")
+
+print("DONE!")
+
+YourAge = GetAge()
